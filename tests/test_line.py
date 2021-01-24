87086faec7a565924944db5cb023ffdd49ce0c71
Adam Hopkins <admhpkns@gmail.com>
2021-01-24 13:30:46 +0200

    Add simple line test

---
+++ tests/test_line.py
@@ -0,0 +1,14 @@
+import pytest
+from sanic_routing.line import Line
+
+
+@pytest.mark.parametrize(
+    "line,expected",
+    (
+        (Line("foo", 0), "foo\n"),
+        (Line("foo", 1), "    foo\n"),
+        (Line("foo", 2), "        foo\n"),
+    ),
+)
+def test_proper_indentation(line, expected):
+    assert str(line) == expected
