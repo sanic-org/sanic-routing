9a42fbfc9e2affffe0e60afda1453ec2a6affc5b
Adam Hopkins <admhpkns@gmail.com>
2021-04-04 16:16:55 +0300

    Test cascading

---
+++ tests/test_router_source.py
@@ -0,0 +1,27 @@
+import pytest
+from sanic_routing import BaseRouter
+
+
+class Router(BaseRouter):
+    def get(self, path, method):
+        return self.resolve(path=path, method=method)
+
+
+@pytest.mark.parametrize(
+    "cascade,lines,not_founds",
+    (
+        (True, 32, 6),
+        (False, 30, 4),
+    ),
+)
+def test_route_correct_coercion(cascade, lines, not_founds):
+    def handler():
+        ...
+
+    router = Router(cascade_not_found=cascade)
+    router.add("/<one>", handler)
+    router.add("/<one>/two/three", handler)
+
+    router.finalize()
+    assert router.find_route_src.count("\n") == lines
+    assert router.find_route_src.count("raise NotFound") == not_founds
