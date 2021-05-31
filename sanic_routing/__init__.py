43143d61c6dd67caf8f9ecf95bcc54d7706921cd
Adam Hopkins <adam@amhopkins.com>
2021-05-31 20:37:41 +0300

    Set release-candidate v0.7

--- sanic_routing/__init__.py
+++ sanic_routing/__init__.py
@@ -2,5 +2,5 @@ from .group import RouteGroup
 from .route import Route
 from .router import BaseRouter
 
-__version__ = "0.6.2"
+__version__ = "0.7.0rc1"
 __all__ = ("BaseRouter", "Route", "RouteGroup")
