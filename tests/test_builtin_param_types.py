c2ecea94109aa2a26d25b93dbfaf5b406e06544a
Adam Hopkins <adam@amhopkins.com>
2022-02-24 11:56:30 +0200

    [BREAKING] Only match non-empty string (#58)

--- tests/test_builtin_param_types.py
+++ tests/test_builtin_param_types.py
@@ -1,5 +1,6 @@
-import pytest
+from unittest.mock import Mock
 
+import pytest
 from sanic_routing import BaseRouter
 from sanic_routing.exceptions import NotFound
 
@@ -131,3 +132,103 @@ def test_correct_slug_v_string(handler):
 
     assert isinstance(retval, str)
     assert retval == "FooBar"
+
+
+@pytest.mark.parametrize(
+    "value,matches",
+    (
+        ("foo", True),
+        ("FooBar", True),
+        ("with123456789", True),
+        ("", False),
+    ),
+)
+def test_nonempty_string(handler, value, matches):
+    def test(path):
+        nonlocal handler
+        router = Router()
+
+        router.add(path, handler)
+        router.finalize()
+
+        if matches:
+            _, handler, params = router.get(f"/{value}", "BASE")
+            retval = handler(**params)
+
+            assert isinstance(retval, str)
+            assert retval == value
+        else:
+            with pytest.raises(NotFound):
+                router.get(f"/{value}", "BASE")
+
+    for path in ("/<foo>", "/<foo:str>"):
+        test(path)
+
+
+@pytest.mark.parametrize(
+    "value",
+    (
+        "foo",
+        "FooBar",
+        "with123456789",
+        "",
+    ),
+)
+def test_empty_string(handler, value):
+    router = Router()
+
+    router.add("/<foo:strorempty>", handler)
+    router.finalize()
+
+    _, handler, params = router.get(f"/{value}", "BASE")
+    retval = handler(**params)
+
+    assert isinstance(retval, str)
+    assert retval == value
+
+
+def test_nonempty_hierarchy():
+    handler1 = Mock()
+    handler2 = Mock()
+    router = Router()
+
+    router.add("/one/<foo>", handler1)
+    router.add("/one/<foo>/<bar>", handler2)
+    router.finalize()
+
+    _, handler, params = router.get("/one/two/", "BASE")
+    expected = {"foo": "two"}
+    handler(**params)
+
+    assert params == expected
+    handler1.assert_called_once_with(**expected)
+    handler2.assert_not_called()
+
+    handler1.reset_mock()
+    handler2.reset_mock()
+
+    _, handler, params = router.get("/one/two/three/", "BASE")
+    expected = {"foo": "two", "bar": "three"}
+    handler(**params)
+
+    assert params == expected
+    handler1.assert_not_called()
+    handler2.assert_called_once_with(**expected)
+
+
+def test_empty_hierarchy():
+    handler1 = Mock()
+    handler2 = Mock()
+    router = Router()
+
+    router.add("/one/<foo>", handler1)
+    router.add("/one/<foo>/<bar:strorempty>", handler2)
+    router.finalize()
+
+    _, handler, params = router.get("/one/two/", "BASE")
+    expected = {"foo": "two", "bar": ""}
+    handler(**params)
+
+    assert params == expected
+    handler1.assert_not_called()
+    handler2.assert_called_once_with(**expected)
