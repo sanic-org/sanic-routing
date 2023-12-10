a8cfae7d91446ce5e2f67a70e22ddf4f931220ca
Adam Hopkins <adam@amhopkins.com>
2023-12-10 13:38:43 +0200

    Make pretty

--- sanic_routing/exceptions.py
+++ sanic_routing/exceptions.py
@@ -25,7 +25,7 @@ class NoMethod(BaseException):
         message: str = "Method does not exist",
         method: Optional[str] = None,
         allowed_methods: Optional[Set[str]] = None,
-        path: Optional[str] = None
+        path: Optional[str] = None,
     ):
         super().__init__(message)
         self.method = method
